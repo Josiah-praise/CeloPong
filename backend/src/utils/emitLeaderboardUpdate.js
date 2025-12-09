d446b8dadd6a3777f9077fbd4e9640b7f2c521dc
Josiah Praise <praisechinonso21@gmail.com>
2025-12-09 15:13:30 +0100

    chore: annotate legacy leaderboard event

--- backend/src/utils/emitLeaderboardUpdate.js
+++ backend/src/utils/emitLeaderboardUpdate.js
@@ -3,6 +3,7 @@ const SocketEvents = require('./socketEvents');
 /**
  * Set of outbound events that should receive identical leaderboard payloads
  */
+// TODO: remove `rankingsUpdate` once telemetry confirms no legacy listeners
 const LEGACY_EVENTS = [
   SocketEvents.LEADERBOARD_UPDATE,
   SocketEvents.RANKINGS_UPDATE,
