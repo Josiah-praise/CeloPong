a21ba198c2bf47efcf37da056d169986ea21f7cd
Josiah Praise <praisechinonso21@gmail.com>
2025-12-09 14:50:43 +0100

    feat: expose leaderboard socket event constants

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -7,4 +7,10 @@ export const PADDLE_SPEED = 0.01;
 export const INITIAL_RATING = 1000;
 
 // Backend connection
-export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL; 
+export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+
+export const SOCKET_EVENTS = Object.freeze({
+  LEADERBOARD_UPDATE: 'leaderboardUpdate',
+  LEGACY_RANKINGS_UPDATE: 'rankingsUpdate',
+  GET_LEADERBOARD: 'getLeaderboard',
+});
