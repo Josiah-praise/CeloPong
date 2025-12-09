e9188df47c03a708c6b125b47643560bc59085ef
Josiah Praise <praisechinonso21@gmail.com>
2025-12-10 00:04:46 +0100

    feat: seed default dev origins

--- backend/src/utils/corsOrigins.js
+++ backend/src/utils/corsOrigins.js
@@ -1,5 +1,10 @@
+const DEFAULT_DEV_ORIGINS = [
+  'http://localhost:3000',
+  'http://127.0.0.1:3000',
+];
+
 function getCorsOrigins() {
-  return [];
+  return DEFAULT_DEV_ORIGINS;
 }
 
 module.exports = {
